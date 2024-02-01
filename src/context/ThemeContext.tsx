9747a97aceb98a76621247317942cb4831eb57e4
Jarrett Lyon <64499632+jlyon12@users.noreply.github.com>
2024-01-31 17:47:30 -0700

    Create theme context and hook

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, useReducer } from 'react';
+
+type FontFamily = 'sans' | 'serif' | 'mono';
+
+interface ThemeContextType {
+	state: InitialStateType;
+	dispatch: React.Dispatch<ACTIONTYPE>;
+}
+
+interface InitialStateType {
+	font: FontFamily;
+	darkMode: boolean;
+}
+
+type ACTIONTYPE =
+	| { type: 'SET_FONT'; payload: FontFamily }
+	| { type: 'TOGGLE_DARKMODE' };
+
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const initialState: InitialStateType = {
+	font: 'sans',
+	darkMode: prefersDark,
+};
+
+const themeReducer = (state: InitialStateType, action: ACTIONTYPE) => {
+	switch (action.type) {
+		case 'SET_FONT':
+			return {
+				...state,
+				font: action.payload,
+			};
+		case 'TOGGLE_DARKMODE':
+			return {
+				...state,
+				darkMode: !state.darkMode,
+			};
+		default:
+			return state;
+	}
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+const ThemeContextProvider = ({ children }: React.PropsWithChildren) => {
+	const [state, dispatch] = useReducer(themeReducer, initialState);
+	return (
+		<ThemeContext.Provider value={{ state, dispatch }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export { ThemeContext, ThemeContextProvider };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { ThemeContextProvider } from './context/ThemeContext.tsx';
 import './sass/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeContextProvider>
+			<App />
+		</ThemeContextProvider>
 	</React.StrictMode>
 );
